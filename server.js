82fffffec4a7536ed2da29cd9cf25bb984aa83d8
smurf1es <pahuger_puruhita_27rpl@student.smktelkom-mlg.sch.id>
2020-03-19 17:53:24 +0700

    Connection to the database (MongoDB Atlas)

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Connect to Database
+connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
