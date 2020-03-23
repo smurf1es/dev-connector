398a09ccf69ad571cd4a2e78dfd0d61d9994c4c0
smurf1es <pahuger_puruhita_27rpl@student.smktelkom-mlg.sch.id>
2020-03-23 21:54:53 +0700

    installed uuid, added alert reducer

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,32 @@ import React, { Fragment } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 
+import Alert from "./components/layout/Alert";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 const App = () => (
-  <BrowserRouter>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Alert />
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,11 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-const Register = () => {
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
+
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -20,7 +24,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Passwords do not match");
+      setAlert("Passwords do not match", "danger");
     } else {
       console.log("Success");
     }
@@ -86,4 +90,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
