16befbb4599bd819feb8a3ab088a74f10bab84ff
smurf1es <pahuger_puruhita_27rpl@student.smktelkom-mlg.sch.id>
2020-04-03 21:30:03 +0700

    get profile by id initial setups

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
@@ -46,6 +47,7 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
