c344b673aa51e082b8c393da2c29f4bfa1647ac1
smurf1es <pahuger_puruhita_27rpl@student.smktelkom-mlg.sch.id>
2020-04-03 23:02:07 +0700

    done profile education section. added github

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,8 @@ import { getProfileById } from "../../actions/profile";
 import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
+import ProfileEducation from "./ProfileEducation";
+import ProfileGithub from "./ProfileGithub";
 
 const Profile = ({
   getProfileById,
@@ -51,6 +53,24 @@ const Profile = ({
                 <h4>No experience credentials</h4>
               )}
             </div>
+
+            <div className="profile-edu bg-white p-2">
+              <h2 className="text-primary">Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map((education) => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credentials</h4>
+              )}
+            </div>
+
+            {profile.githubusername && <ProfileGithub />}
           </div>
         </Fragment>
       )}
